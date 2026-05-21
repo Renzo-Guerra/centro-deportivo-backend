abdd5e50fcb592bb97e090d660a6e381ada8795c
Renzo Guerra <renzoguerralatt@gmail.com>
2026-05-21 19:32:13 -0300

    CONFIG: When to execute seeder
    El seeder se ejecutaba SIEMPRE, lo cual era util con create-drop, pero ahora que es
    update genera duplicate KEY por los UNIQUEs, por ende solo se ejecutara cuando la base de
    datos NO tiene roles asignados. De lo contrario poblara las respectivas tablas

--- src/main/java/org/learning/sistemacanchas/bootstrap/DatabaseSeeder.java
+++ src/main/java/org/learning/sistemacanchas/bootstrap/DatabaseSeeder.java
@@ -1,6 +1,7 @@
 package org.learning.sistemacanchas.bootstrap;
 
 import lombok.RequiredArgsConstructor;
+import org.learning.sistemacanchas.repository.RolRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -12,9 +13,14 @@ public class DatabaseSeeder implements ApplicationListener<ContextRefreshedEvent
     private final UsuarioSeeder usuarioSeeder;
     private final CanchaSeeder canchaSeeder;
     private final TurnoSeeder turnoSeeder;
+    private final RolRepository rolRepository;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
+        if(rolRepository.count() > 0){
+            return;
+        }
+
         rolSeeder.seed();
         usuarioSeeder.seed();
         canchaSeeder.seed();
